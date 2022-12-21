cab0011284a9fa28ce72780f4b773c3d5edea877
Lukman Hawi <lukmanhawi@gmail.com>
2022-12-21 13:08:16 +0700

    Add business logic

--- src/TodolistApp.java
+++ src/TodolistApp.java
@@ -1,5 +1,20 @@
 public class TodolistApp {
+
+    public static String[] model = new String[15];
+
     public static void main(String[] args) {
+
+    }
+
+    public static void showTodoList(){
+
+    }
+
+    public static void addTodoList(){
+
+    }
+
+    public static void deleteTodoList(){
 
     }
 }
